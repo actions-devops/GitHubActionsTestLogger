b383a0db6befe1885adabe841854d1a855ea703b
Alexey Golub <tyrrrz@gmail.com>
2020-03-22 19:03:10 +0200

    More examples

--- Playground/UnitTest1.cs
+++ Playground/UnitTest1.cs
@@ -10,5 +10,16 @@ namespace Playground
 
         [Fact]
         public void Test2() => Assert.True(1 == 2);
+
+        private void ThrowException() => throw new InvalidOperationException("test");
+
+        [Fact]
+        public void Test3() => ThrowException();
+
+        [Fact]
+        public void Test4() => Assert.Contains(new[] {1, 2, 3}, i => i == 4);
+
+        [Fact(Skip = "Skipped for some reason")]
+        public void Test5() => Assert.True(true);
     }
 }
